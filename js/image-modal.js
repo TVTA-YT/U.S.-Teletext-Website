62728cb725b6076dfae42eedf19cd367b3230e4e
Dylan Nickels <filmcollecting12@gmail.com>
2026-08-12 02:32:18 -0400

    Updates (August 12, 2026)
    **HTML**
    
    Homepage
    - Updated first introduction paragraph to correct a typographical error (space, followed by comma).
    
    About / Archiving Teletext / Sitemap / Teletext Links (multiple pages)
    - Removed placeholder text. Page now has content.
    
    Electra / ExtraVision / NBC Teletext Results (multiple pages)
    - Added button that allows users to navigate back to the initial form.
    
    Electra
    - Added option for 1987.
    
    Services
    - Update footer logo; the placeholder logo was still being used.
    
    **JS**
    
    ***NEW FILE***
    - Imported pre-existing Image Modal JS file from another project. This will open images inside a modal when clicked.
    
    Render Results
    - Added the Bootstrap class "text-nowrap" to the "table" element.
    
    **Other**
    - Added image showing Electra teletext inside the VBI during an NBA Finals broadcast on TBS on June 3, 1986.

---
+++ js/image-modal.js
@@ -0,0 +1,18 @@
+// ~ Bootstrap image modal.
+
+let modal = new bootstrap.Modal(document.getElementById("imageModal"));
+
+document.addEventListener("click", (event) => {
+  const item = event.target.closest(
+    ".figure-img",
+  );
+  if (!item) return;
+
+  const src = item.getAttribute("src");
+  const caption = item.getAttribute("data-bs-caption") || "Default caption.";
+
+  document.getElementById("modalImage").src = src;
+  document.getElementById("modalCaption").innerText = caption;
+
+  modal.show();
+});
